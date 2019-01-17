296ed392c7b02f741d14cbe5e712fadb9dfa27e2
Hanif Tiznobake <hanif@tiznobake.com>
2019-01-17 02:30:30 -0800

    Localization

--- HT_Localizer.m
+++ HT_Localizer.m
@@ -1,22 +1,22 @@
-function source = HT_Localizer(activities)
-%HT_LOCALIZER Finds location and time of an incident based on acoustic
-%observations in N points (N>=4).
+function activities = HT_Localizer(detections)
+%HT_LOCALIZER Finds location and time of an activity based on acoustic
+%detections in N points (N>=4).
 
-%   activities (N by 4 matrix):
+%   detections (N by 4 matrix):
 %       N: Number of observations
 %       4: Latitude, Longitude, Time, Temperature
-%   sources (M by 4 matrix):
+%   activities (M by 4 matrix):
 %       M: Number of potentially valid localizations.
 %       4: Latitude, Longitude, Time, Error
 
 % © 2018 Hanif Tiznobake
 
-c = 331.3+0.606*mean(activities(:,4),1);                                    % Speed of sound:  c = 331.3+0.606*theta (taken from https://en.wikipedia.org/wiki/Speed_of_sound at 11/11/2018)
+c = 331.3+0.606*mean(detections(:,4),1);                                    % Speed of sound:  c = 331.3+0.606*theta (taken from https://en.wikipedia.org/wiki/Speed_of_sound at 11/11/2018)
 [x,y,~]=geodetic2enu(...
-    activities(:,1),activities(:,2),zeros(size(activities,1),1),...
-    activities(1,1),activities(1,2),0,referenceEllipsoid('earth'));
-t=activities(:,3);
-v=nchoosek(1:size(activities,1),3);
+    detections(:,1),detections(:,2),zeros(size(detections,1),1),...
+    detections(1,1),detections(1,2),0,referenceEllipsoid('earth'));
+t=detections(:,3);
+v=nchoosek(1:size(detections,1),3);
 sol=[];
 syms x0 y0 t0
 for i=1:size(v,1)
@@ -30,9 +30,9 @@ for i=1:size(v,1)
         sol=[sol;a];
     end
 end
-sol = sol(sol(:,3)<min(t),:);
-source = zeros(size(sol,1),4);
-[source(:,1),source(:,2),~] = enu2geodetic(...
+sol = unique(sol(sol(:,3)<min(t) & sol(:,3)~=0,:),'rows');
+activities = zeros(size(sol,1),4);
+[activities(:,1),activities(:,2),~] = enu2geodetic(...
     sol(:,1),sol(:,2),zeros(size(sol,1),1),...
-    activities(1,1),activities(1,2),0,referenceEllipsoid('earth'));
-source(:,3)=sol(:,3);
+    detections(1,1),detections(1,2),0,referenceEllipsoid('earth'));
+activities(:,3)=sol(:,3);
