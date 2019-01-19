1a86930825549000e4965b8002eaec356b0dcca9
Joel Reznick <32008471+bozingle@users.noreply.github.com>
2019-01-19 10:38:56 -0600

    update final
    JR_Data's finish. Comments at the top will describe the data types implementation and how to use it. A test file will also be provided to demonstrate the implementation.

--- JR_Data.m
+++ JR_Data.m
@@ -1,6 +1,13 @@
 classdef JR_Data
     %UNTITLED3 Summary of this class goes here
-    %   Detailed explanation goes here
+    %To invoke the constructor:
+    %   obj = JR_Data(fileDir+fileName+audio extension(i.e ".wav"))
+    % To retrieve intervals of time from memory
+    %   obj.get(first, last)
+    %first and last are in seconds. Any interval works.
+    %first - the start of the interval to load from memory.
+    %last - the end of the interval to load from memory.
+    
     
     properties
         audio
@@ -10,7 +17,7 @@ classdef JR_Data
         scale
         progress
         filepath
-        TAList
+        finalTime
     end
     
     methods
@@ -44,7 +51,7 @@ classdef JR_Data
             first = round(0.1*obj.scale*obj.fs);
             last = length(obj.audio.audio);
             obj.progress = 0;
-            obj.TAList = [];
+            spgramTA = tall([]);
             for (i = first*mult:first*mult:last)
                 
                 [s,~,t1] = spectrogram(obj.audio.audio((i-first*mult+1):i),first,...
@@ -56,65 +63,25 @@ classdef JR_Data
                 sLength = length(s(1,:));
                 spgramA = t1; 
                 spgramA(:,2:sLength+1) = s;
-                indc = length(s(:,1))*iter;
-                indpr = indc - length(s(:,1))+1;
-                
-                spgramTA = tall(spgramA);
-                mkdir(obj.filepath+"\TA"+t1(end));
-                write(obj.filepath+"\TA"+t1(end)+"\TallA"+iter+"_*.mat",spgramTA,'FileType', 'mat');
-                obj.TAList(iter) = t1(end);
+                spgramTA = tall([spgramTA;tall(spgramA)]);
                 disp("Progress: " + obj.progress + "%");
                 obj.progress = round((i/last)*10000)/100;
+                obj.finalTime = t1(end);
             end
+            write(obj.filepath+"\TA",spgramTA,'FileType', 'mat');
             disp("Complete!");
         end
         
-        function [s,t] = get(obj, last)
+        function [s,t] = get(obj, first, last)
             %Loads file with the last second closest to the last value
-            TASelectMax = abs(obj.TAList - last);
-            minTA = min(TASelectMax);
-            TASelectMax = find(TASelectMax == minTA);
-            fileDir = dir(obj.filepath+"\TA" + obj.TAList(TASelectMax));
-            fileName = [fileDir.name];
-            fileName = erase(fileName,"...");
-            file = load(obj.filepath+"\TA" + obj.TAList(TASelectMax)+"\"+fileName);
-            values = file.Value;
-            
-            vLength = length(values);
-            iLength = length(values{1,1}(:,1));
-            t = [];
-            s = [];
-            [startIndex, iter] = obj.findStart(vLength,values,last);
-            if length(startIndex)>1
-                t = [values{iter,1}(startIndex(2):end,1)];
-            else
-                t = [values{iter,1}(startIndex:end,1)];
-            end
-            for i = iter+1:length(values)
-                    t = [t;values{i,1}(:,1)];
-                    s = [s;values{i,1}(:,2:end)];
-                    DiffTable = abs(t - last);
-                    minDiff = min(DiffTable);
-                    if minDiff < 0.5
-                        break;
-                    end
-            end
-        end
-        
-        function [index,iter] = findStart(obj,vLength, values, last)
-            index = 0;
-            iter = 0;
-            last = last-10;
-            for i = 1:vLength
-                t = values{i,1}(:,1);
-                minValDiffMat = abs(t - last);
-                minValDiff = min(minValDiffMat);
-                if minValDiff < .5
-                    iter = i;
-                    index = find(minValDiff == minValDiffMat);
-                    break;
-                end
-            end
+            ds = datastore(obj.filepath + "\TA");
+            data = tall(ds);
+            idx = data(:,1) <= last;
+            newData = data(idx,:);
+            idx = newData(:,1) >= first;
+            newData = gather(newData(idx,:));
+            t = newData(:,1);
+            s = newData(:,2:end);
         end
         
         function display(obj,graphics,interval)
