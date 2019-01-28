241a6d88054d13d379d67edd99651c9f56fe7452
Joel Reznick <32008471+bozingle@users.noreply.github.com>
2019-01-28 14:59:57 -0600

    20 Second intervals
    This time, there should be no overlap.

--- JR_Data.m
+++ JR_Data.m
@@ -9,12 +9,10 @@ classdef JR_Data
     %last - the end of the interval to load from memory.
     
     properties
-        f
-        fs
         audio
+        fs
         spgram
         scale
-        overlap
         progress
         filepath
         finalTimeAudio
@@ -22,32 +20,19 @@ classdef JR_Data
     end
     
     methods
-        function obj = JR_Data(recording,varargin)
-            p=inputParser;
-            addRequired(p,'recording',@(x) ischar(x));
-            addParameter(p,'scale',0.08,@(x) isnumeric(x));
-            addParameter(p,'overlap',0.8,@(x) isnumeric(x));
-            addParameter(p,'f',0:10:10000,@(x) isnumeric(x));
-            parse(p,recording,varargin{:});
-            
-            obj.filepath = "..\..\..\Quail Call - Datastore\"+erase(p.Results.recording,".wav")+"\";%Location where the processed file should be.
-            if exist(obj.filepath) && isempty(varargin)
-                fileObj = load(obj.filepath+erase(p.Results.recording,".wav")+".mat");
+        function obj = JR_Data(recording)
+            obj.filepath = ""+erase(recording,".wav");%Location where the processed file should be.
+            if exist(obj.filepath)
+                fileObj = load(obj.filepath+"\processed"+erase(recording,".wav")+".mat");
                 obj = fileObj.obj;
                 ds = datastore(obj.filepath + "\spgram");
                 obj.spgram = tall(ds);
                 ds = datastore(obj.filepath+"\audio");
                 obj.audio = tall(ds);
             else
-                try
-                    rmdir(obj.filepath,'s');
-                catch
-                end
-                mkdir(obj.filepath);
-                [obj,raw]=obj.read(p.Results.recording);
-                obj.scale = p.Results.scale;
-                obj.f = p.Results.f;
-                obj.overlap = p.Results.overlap;
+                obj.finalTimeSpgram = 0;
+                [obj,raw]=obj.read(recording);
+                obj.scale=0.8;
                 obj = obj.process(raw);
                 obj = obj.sp();
                 ds = datastore(obj.filepath + "\spgram");
@@ -55,13 +40,13 @@ classdef JR_Data
                 obj = obj.formatAudio();
                 ds = datastore(obj.filepath+"\audio");
                 obj.audio = tall(ds);
-                save(obj.filepath+erase(p.Results.recording,".wav")+".mat", "obj");
+                save(obj.filepath+"\processed"+erase(recording,".wav")+".mat", "obj");
             end
         end
         
         function [obj,raw]=read(obj,recording)
-            path = "..\..\..\Quail Call - Recordings\"+recording;
-            [raw,obj.fs]=audioread(path);
+            filepath = "..\..\..\Quail Call - Recordings\"+recording; %"..\..\..\Quail Call - Recordings\"+
+            [raw,obj.fs]=audioread(filepath);
         end
         
         function obj=process(obj,raw)
@@ -71,33 +56,34 @@ classdef JR_Data
         function obj = formatAudio(obj)
             obj.audio = obj.audio';
             obj.audio(:,2) = obj.audio;
-            obj.finalTimeAudio = (length(obj.audio)/obj.fs);
+            obj.finalTimeAudio = (length(obj.audio)/obj.fs)
             obj.audio(:,1) = ((1/obj.fs):(1/obj.fs):obj.finalTimeAudio)';
             obj.audio = tall(obj.audio);
             write(obj.filepath+"\audio",obj.audio,'FileType', 'mat');
         end
         
         function [obj]=sp(obj)
-            %mult = 250;%125 for 10s intervals (please don't do hard-coded numbers next time)
-            window = round(obj.scale*obj.fs);
-            mult=round(20/obj.scale);
+            mkdir(obj.filepath);
+            f  = 0:10:10000;
+            mult=20*round(10/obj.scale);%125 for 10s intervals
+            first = round(0.1*obj.scale*obj.fs);
             last = length(obj.audio);
             obj.progress = 0;
             spgramTA = tall([]);
             disp("Processing spectrogram");
-            for i = window*mult:window*mult:last
-                [s,~,t1] = spectrogram(obj.audio(max(1,(i-window*(mult+1)-round(obj.overlap*window)+1)):i),window,...
-                    round(obj.overlap*window),obj.f,obj.fs);
+            for (i = first*mult:first*mult:last)
+                [s,~,t] = spectrogram(obj.audio((i-first*mult+1):i),first,...
+                    round(0.8*0.1*obj.scale*obj.fs),f,obj.fs);
                 s = db(abs(s'));
-                iter = i/(window*mult);
-                t1 = t1' + (iter - 1)*20;
+                iter = i/(first*mult);
+                t = t' + obj.finalTimeSpgram;
                 sLength = length(s(1,:));
-                spgramA = t1; 
+                spgramA = t; 
                 spgramA(:,2:sLength+1) = s;
                 spgramTA = tall([spgramTA;tall(spgramA)]);
                 disp("Progress: " + obj.progress + "%");
                 obj.progress = round((i/last)*10000)/100;
-                obj.finalTimeSpgram = t1(end);
+                obj.finalTimeSpgram = t(end);
             end
             write(obj.filepath+"\spgram",spgramTA,'FileType', 'mat');
             disp("Complete!");
