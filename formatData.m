5ff6ad8d2cc3a1991d3fb9761b3b0e1ca5deaec1
Joel Reznick <32008471+bozingle@users.noreply.github.com>
2019-02-02 19:19:06 -0600

    Database Update
    This update lets you load data from the Database instead of an excel spreadsheet now.  test.m demonstrates how you can use this function.

--- formatData.m
+++ formatData.m
@@ -1,216 +1,153 @@
-function formatData()
+function results = formatData(Date, SorE)
 addpath(genpath(pwd));
 %Get the Data
-[mADETStart, mBDETStart, mCDETStart, mDDETStart] = getData("QuailDetections",1, 'start');
-%[mADETEND, mBDETEND, mCDETEND, mDDETEND] = getData("QuailDetections",1, 'end');
-[mAANNStart, mBANNStart, mCANNStart, mDANNStart] = getData("QuailDetections", 2, 'start');
-%[mAANNEND, mBANNEND, mCANNEND, mDANNEND] = getData("QuailDetections", 2, 'start');
-
-
+[channels,posTemp] = getData(Date,SorE);
 
 %Refine the data
-[lagMatrix, refinedMatrixDETStart] = findRefinedMatrix(mADETStart, mBDETStart, mCDETStart(2:end), mDDETStart);
-%     [refinedMatrixANNStart] = findRefinedMatrix(mAANNStart, mBANNStart, mCANNStart, mDANNStart);
+[lagMatrix,refinedMatrix] = findRefinedMatrix(channels);
 
-noEchoesADETStart = removeEchoesSimple(refinedMatrixDETStart(1,:));
-noEchoesBDETStart = removeEchoesSimple(refinedMatrixDETStart(2,:));
-noEchoesCDETStart = removeEchoesSimple(refinedMatrixDETStart(3,:));
-noEchoesDDETStart = removeEchoesSimple(refinedMatrixDETStart(4,:));
-%[lagMatrixDETStart, refinedMatrixDETStart] = removeEchoes(lagMatrixDETStart, refinedMatrixDETStart);
+[lagMatrix,refinedMatrix] = removeEchoes(lagMatrix, refinedMatrix);
 
-%The refined and lag matrix is represented as the following:
-%1st row contains the A elements
-%2nd row contains the B elements
-%3rd row contains the C elements
-
-%compare refined matrices of annotations and detections
-[TPStart, TNStart, FPStart, FNStart] = compareData( noEchoesDDETStart, mDANNStart);
-%[refinedTPEnd, refinedTNEnd, refinedFPEnd, refinedFNEnd] = compareData(refinedMatrixDETEnd, refinedMatrixANNEnd)
-%compare annotations to detections
-mRef = [33.40738 -101.49181 0 40;...
-        33.40739 -101.49127 0 40;...
-        33.40694 -101.49126 0 40;...
-        33.40693 -101.49180 0 40];
-results = [];
-for i = 1:length(refinedMatrixDETStart(1,:))
-    mRef(:,3) = lagMatrix(:, i);
-    [location] = HT_Localizer(mRef);
-    results(i,:) = location(1,:);
+mRef = [];
+for i = 1: length(posTemp)
+    mRef = [mRef;posTemp{i,1}(1:2) 0 posTemp{i,1}(3)];
 end
-MSELat = (1/61)*sum((33.40678 - results(:,1)).^2);
-MSELong = (1/61)*sum((-101.49168 - results(:,2)).^2);
-%compare results of both
+results = [];
+for i = 1:length(lagMatrix(1,:))
+    mRef(:,3) = lagMatrix(:, i);
+    location = HT_Localizer(mRef);
+    results(i,:) = location(1:2);
+end
+
+
 end
 
 %find lag matrix
-function [mA, mB, mC, mD] = getData(filename, sheet, set)
+function [finalChannels,posTemp] = getData(date,SorE)
 
-%Excel for ease
-xLData = xlsread(filename+".xlsx", sheet);
-if (strcmp(set, 'end'))
-    i = 1;
-else
-    i = 0;
-end
-mA = removeNaNSpots(xLData(:,1+i));
-mB = removeNaNSpots(xLData(:,3+i));
-mC = removeNaNSpots(xLData(:,5+i));
-mD = removeNaNSpots(xLData(:,7+i));
+    finalChannels = {};
+    posTemp = {};
+    DataStart = HT_DataAccess([],'query',...
+                        "SELECT Recorders.[Recorder ID], [Detection(s)]."+SorE+" "+...
+"FROM Recorders INNER JOIN ([Detection(s)] INNER JOIN Recordings ON [Detection(s)].[Recording ID] = Recordings.[Recording ID]) ON Recorders.[Recorder ID] = Recordings.[Recorder ID]"+...
+"WHERE ((([Detection(s)].Created)>#"+date+"#))"+...
+    "ORDER BY Recorders.[Recorder ID], [Detection(s)]."+SorE+";",'numeric');
+%SELECT Recorders.[Recorder ID], [Detection(s)].Start
+%FROM Recorders INNER JOIN ([Detection(s)] INNER JOIN Recordings ON [Detection(s)].[Recording ID] = Recordings.[Recording ID]) ON Recorders.[Recorder ID] = Recordings.[Recorder ID]
+%WHERE ((([Detection(s)].Created)>#1/2/2019#))
+%ORDER BY Recorders.[Recorder ID], [Detection(s)].Start;
+    DataposTemp = HT_DataAccess([],'query',...
+                            "SELECT Recorders.[Recorder ID], Statuses.LAT, Statuses.LON, Statuses.[TEMP(C)]"+...
+    "FROM (Recorders INNER JOIN ([Detection(s)] INNER JOIN Recordings ON [Detection(s)].[Recording ID] = Recordings.[Recording ID]) ON Recorders.[Recorder ID] = Recordings.[Recorder ID]) INNER JOIN Statuses ON Recorders.[Recorder ID] = Statuses.[Recorder ID]"+...
+    "WHERE ((([Detection(s)].Created)>#"+date+"#))"+...
+    "ORDER BY Recorders.[Recorder ID], [Detection(s)]."+SorE+";",'numeric');
 
-%For Database(Not working)
-%conn = database("Quail", "", "");
-%load files(currently temp matrices.)
-%mA = databaseImportOptions(conn, ['SELECT Annotations.[Start Time], Annotations.[End Time]'...
-%    'FROM Annotations'...
-%    'WHERE (((Annotations.[Channel ID])=17) AND ((Annotations.[Created By]) Is Null))'...
-%    'ORDER BY Annotations.[Start Time], Annotations.[End Time];'])
-%mB = HT_DataAccess([], "query",...
-%    ['SELECT Annotations.[Start Time], Annotations.[End Time], Annotations.[Channel ID]'...
-%    'FROM Annotations'...
-%    'WHERE (((Annotations.[Channel ID])=19) AND ((Annotations.[Created By]) Is Null) AND ((Annotations.DateTime)=#10/4/2018 11:30:0#))'...
-%    'ORDER BY Annotations.[Start Time], Annotations.[End Time];'],'cellarray');
-%mC = HT_DataAccess([], "query",...
-%    ['SELECT Annotations.[Start Time], Annotations.[End Time], Annotations.[Channel ID]'...
-%    'FROM Annotations'...
-%    'WHERE (((Annotations.[Channel ID])=21) AND ((Annotations.[Created By]) Is Null) AND ((Annotations.DateTime)=#10/4/2018 11:30:0#))'...
-%    'ORDER BY Annotations.[Start Time], Annotations.[End Time];'],'cellarray');
-%mD = HT_DataAccess([], "query",...
-%    ['SELECT Annotations.[Start Time], Annotations.[End Time], Annotations.[Channel ID]'...
-%    'FROM Annotations'...
-%    'WHERE (((Annotations.[Channel ID])=23) AND ((Annotations.[Created By]) Is Null) AND ((Annotations.DateTime)=#10/4/2018 11:30:0#))'...
-%    'ORDER BY Annotations.[Start Time], Annotations.[End Time];'],'cellarray');
+%SELECT Recorders.[Recorder ID], Statuses.LAT, Statuses.LON, Statuses.[TEMP(C)]
+%FROM (Recorders INNER JOIN ([Detection(s)] INNER JOIN Recordings ON [Detection(s)].[Recording ID] = Recordings.[Recording ID]) ON Recorders.[Recorder ID] = Recordings.[Recorder ID]) INNER JOIN Statuses ON Recorders.[Recorder ID] = Statuses.[Recorder ID]
+%WHERE ((([Detection(s)].Created)>#1/2/2019#))
+%ORDER BY Recorders.[Recorder ID], [Detection(s)].Start;
 
-
-%Refine the matrix (remove timestamps that do not correspond with the
-%other recordings by
-
-
+    i = 1
+    while(length(DataStart(:,1))>0)
+        idx = find(DataStart(:,1) == DataStart(1,1));
+        finalChannels{i,1} = DataStart(idx,2);
+        DataStart(idx,:) = [];
+        idx = find(DataposTemp(:,1) == DataposTemp(1,1));
+        posTemp{i,1} = DataposTemp(idx,2:end);
+        posTemp{i,1} = mean(posTemp{i,1});
+        DataposTemp(idx, :) = [];
+        i = i + 1;
+    end
 end
 
 %Refine the matrices from the files into lag matrices
 %what we want the output to be for current time values.
-function [lagMatrix, refinedMatrix] = findRefinedMatrix(mA,mB,mC, mD)
+function [lagMatrix,refinedMatrix] = findRefinedMatrix(channels)
 
-%Maximum distance in time from the reference point.
+    %Maximum distance in time from the reference point.
 
-maxTDistance = 0.6;
-
-%Define refinedMatrix as empty for now
-
-refinedMatrix = [];
-
-%Initialize i as 1 so that it will reference the first column in each
-%of the provided matrices.
-
-i = 1;
-
-%The loop considers if the dataset was incomplete.
-while (i <= length(mA) && i <= length(mB) && i <= length(mC) && i <= length(mD))
-    
-    
-    columnN = [mA(i); mB(i); mC(i); mD(i)];
-    
-    %Find reference point of columnN
-    
-    ref = min(columnN);
-    
-    %Find a "truth table" named tTable that says where the condition
-    %was met.
-    
-    tTable = columnN - ref > maxTDistance;
-    
-    
-    %Average the truth values (0 and 1)
-    
-    avgMaxT = mean(tTable);
-    
-    %If the avg > 0 that means that the condition was met by at least
-    %one of the values met the condition.
-    if (avgMaxT > 0)
-        
-        %Find the row numbers of the smallest values
-        
-        %                rowNums = find(columnN < max(columnN));
-        rowNums = find(tTable == 0);
-        %Depending on which row numbers were chosen by the find
-        %function, delete those row positions.
-        
-        if (find(rowNums == 1))
-            mA(i) = [];
-        end
-        if (find(rowNums == 2))
-            mB(i) = [];
-        end
-        if (find(rowNums == 3))
-            mC(i) = [];
-        end
-        if (find(rowNums == 4))
-            mD(i) = [];
-        end
-    else
-        
-        %If everything was fine with the column, append the columnN to
-        %the refined matrix, then increment i.
-        
-        
-        refinedMatrix(:,i) = columnN;
-        lagMatrix(:,i) = columnN - ref;
-        %                 lagMatrix(:,i) = columnN - ref;
-        i = i+1;
+    maxTDistance = 0.3;
+    numChannels = length(channels);
+    lengths = [];
+    for j = 1:numChannels
+        lengths(j) = length(channels{j,1});
     end
-end
-end
+    
+    lagMatrix = [];
+    refinedMatrix = [];
+    
+    %Initialize i as 1 so that it will reference the first column in each
+    %of the provided matrices.
 
-%Remove NaNs at the end of the matrix provided.
-function mA = removeNaNSpots(mA)
-nanSpots = find(isnan(mA) == 1);
-for i=1:length(nanSpots)
-    mA(nanSpots(1)) = [];
-end
-end
+    i = 1;
 
-function [TP, TN, FP, FN] = compareData(DET, ANN)
-%find TP
-TP = 0;
-TN = 0;
-FN = 0;
-FP = 0;
-for incJ = 1:length(ANN)
-    mTable = abs(DET-ANN(incJ));
-    ref = min(mTable);
-    %         disp("ref: "+ref);
-    %         disp("ANN: "+ANN(incJ));
-    %         disp("DET: "+DET(find(mTable == ref)));
-    if ref < 0.2
-        TP = TP + 1;
+    %The loop considers if the dataset was incomplete.
+    while ((sum([0 ~= lengths])/numChannels) >=1)
+        columnN = [];
+        for j = 1:numChannels
+            try
+            columnN(j) = channels{j,1}(1);
+            catch
+                disp("Stopped");
+            end
+        end
+        columnN = columnN';
+        %Find reference point of columnN
+
+        ref = min(columnN);
+
+        %Find a "truth table" named tTable that says where the condition
+        %was met.
+
+        tTable = columnN - ref > maxTDistance;
+
+
+        %Average the truth values (0 and 1)
+
+        avgMaxT = mean(tTable);
+
+        %If the avg > 0 that means that the condition was met by at least
+        %one of the values met the condition.
+        if (avgMaxT > 0)
+
+            %Find the row numbers of the smallest values
+
+            rowNums = find(columnN < max(columnN));
+            %rowNums = find(tTable == 0);
+            %Depending on which row numbers were chosen by the find
+            %function, delete those row positions.
+            for j=1:length(rowNums)
+                channels{rowNums(j),1}(1) = [];
+            end
+            for j = 1:numChannels
+                lengths(j) = length(channels{j,1});
+            end
+        else
+
+            %If everything was fine with the column, append the columnN to
+            %the refined matrix, then increment i.
+            lagMatrix(:,i) = columnN - ref;
+            refinedMatrix(:,i) = columnN;
+            
+            for j = 1:numChannels
+                channels{j,1}(1) = [];
+            end
+            i = i + 1;
+        end
     end
 end
 
-FP = length(DET) - TP;
-FN = length(ANN) - TP;
-end
-
-function [lagMatrix, refinedMatrix] = removeEchoes(lMatrix, rMatrix)
-deltaRThresh = 0.4;
-for i = 1:(length(lMatrix(1,:))/2 - 1)
-    deltaR =  mean(abs(rMatrix(:,i+1) - rMatrix(:, i)));
-    if deltaR < deltaRThresh
-        lMatrix(:,i+1) = [];
-        rMatrix(:,i+1) = [];
-    end
-end
-lagMatrix = lMatrix;
-refinedMatrix = rMatrix;
-end
-
-function [noEchoes] = removeEchoesSimple(DET)
+function [lagMatrix,refinedMatrix] = removeEchoes(lMatrix, rMatrix)
+deltaRThresh = 0.7;
 i=1;
-while( i <length(DET))
-    if abs(DET(i) - DET(i+1)) < 0.7
-        DET(i+1) = [];
+while( i <length(lMatrix))
+    if abs(rMatrix(1,i) - rMatrix(1,i+1)) < deltaRThresh
+        rMatrix(:,i+1) = [];
+        lMatrix(:,i+1) = [];
     else
         i=i+1;
     end
 end
-noEchoes = DET;
+lagMatrix = lMatrix;
+refinedMatrix = rMatrix;
 end
