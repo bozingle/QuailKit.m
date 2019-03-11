b3a137c75217af9ab449200987fde19a33af199b
Hanif Tiznobake <haniov@outlook.com>
2019-03-10 23:04:35 -0500

    batch

---
+++ batchdata.m
@@ -0,0 +1,9 @@
+addpath('JR_QuailKit');
+path="Z:\QuailKit";
+d=dir(fullfile(path,'audio'));
+for i=1:length(d)
+    if ~d(i).isdir
+        n=erase(d(i).name,'.wav');
+        JR_Data(fullfile(d(i).folder,d(i).name),fullfile(path,'data',[n,'.h5']));
+    end
+end
