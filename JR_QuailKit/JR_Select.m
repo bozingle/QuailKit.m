e1a621fc62a2f4a94cecc5bef9b4e5cb5ab1d2e9
Hanif Tiznobake <haniov@outlook.com>
2019-03-30 19:56:42 -0500

    REMOVED

--- JR_QuailKit/JR_Select.m
+++
@@ -1,8 +0,0 @@
-function handles = JR_Select(handles, selection)
-    handles.Data.Audio=audioplayer(handles.Data.TS.Data(...
-    handles.Data.Edges(handles.Data.j):...
-    handles.Data.Edges(handles.Data.j+1),selection),handles.Data.fs);
-    handles.Data.Audio.TimerFcn={@TimerFcn, handles};
-    handles.Data.Audio.TimerPeriod=0.05;
-    handles.Data.F=handles.UserData.Freq(1):10:handles.UserData.Freq(2);
-end
