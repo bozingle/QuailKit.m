a58a06239f755a6ab31782d4929b88371ba86f25
Hanif Tiznobake <haniov@outlook.com>
2019-04-07 02:35:44 -0500

    minor

--- HTstpsd_sample.m
+++ HTstpsd_sample.m
@@ -6,5 +6,5 @@ overlap=0.8;
 freqs=[0,10000,10];
 signal=audio(interval(1)*fs-floor(scale*fs/2)+1:interval(2)*fs+floor(scale*fs/2),ch); % This will produce the same exact interval as the asked interval (compnesation for window size)
 [s,t,props] = HTstpsd(signal,fs,'scale',scale,'overlap',overlap,'freqs',freqs);
-t=t+interval(1)-floor(0.08*fs)/2/fs;
+t=t+interval(1)+(1-floor(scale*fs/2))/fs;
 surf(t,freqs(1):freqs(3):freqs(2),db(s,'power'),'edgecolor','none');view(0,90)
