756f8c90d7ee7aca139b5f4ff9f60c0fba24ffcf
Hanif Tiznobake <haniov@outlook.com>
2019-04-07 21:04:21 -0500

    spgram sampling problem solved

--- HTstpsd_sample.m
+++ HTstpsd_sample.m
@@ -1,10 +1,18 @@
-[audio,fs]=audioread('C:\Users\joggl\Texas Tech University\Quail Call - Recordings\SM304472_0+1_20181004$110000.wav');
+[audio,fs]=audioread('Z:\QuailKit\audio\SM304472_0+1_20181219$100000.wav');
 ch=1;
-interval=[120,130];
+interval=[0,40];
 scale=0.08;
 overlap=0.8;
 freqs=[0,10000,10];
-signal=audio(interval(1)*fs-floor(scale*fs/2)+1:interval(2)*fs+floor(scale*fs/2),ch); % This will produce the same exact interval as the asked interval (compnesation for window size)
-[s,t,props] = HTstpsd(signal,fs,'scale',scale,'overlap',overlap,'freqs',freqs);
-t=t+interval(1)+(1-floor(scale*fs/2))/fs;
+samples=max(round((1-overlap)*scale*fs/2)+1,interval(1)*fs-round(scale*fs/2)+1):min(size(audio,1),interval(2)*fs+round(scale*fs/2));
+signal=audio(samples,ch); % This will produce the same exact interval as the asked interval (compnesation for window size)
+[s1,t,props] = HTstpsd(signal,fs,'scale',scale,'overlap',overlap,'freqs',freqs);
+t1=t+samples(1)/fs;
+interval=[40,80];
+samples=max(round((1-overlap)*scale*fs/2)+1,interval(1)*fs-round(scale*fs/2)+1):min(size(audio,1),interval(2)*fs+round(scale*fs/2));
+signal=audio(samples,ch); % This will produce the same exact interval as the asked interval (compnesation for window size)
+[s2,t,props] = HTstpsd(signal,fs,'scale',scale,'overlap',overlap,'freqs',freqs);
+t2=t+samples(1)/fs;
+t=[t1,t2];
+s=[s1,s2];
 surf(t,freqs(1):freqs(3):freqs(2),db(s,'power'),'edgecolor','none');view(0,90)
