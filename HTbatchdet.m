2d9cbc6d9cc85a9cdae9751fec58097f8f05d181
Hanif Tiznobake <haniov@outlook.com>
2019-04-09 02:00:51 -0500

    bacthdet complete
    modified SHdet too.
    h5 files have problem of indexing into nothing still.

--- HTbatchdet.m
+++ HTbatchdet.m
@@ -1,27 +1,22 @@
-addpath('JR_QuailKit');
-path="Z:\QuailKit";
-d=dir(fullfile(path,'data'));
-for i=1:length(d)
-    if ~d(i).isdir
-        obj=JR_Data("",fullfile(d(i).folder,d(i).name),40,[0,10000,10],0.08);
-        k=0;
-        Calls=[];
-        while true
-            k=k+1;
+d=HT_DataAccess([],'query',[...
+    'select name, stream_id from dbo.data',...
+    ],'cellarray');
+conn = database('QuailKit','QuailKit','Q123456789');
+for i=1:size(d,1)
+    obj=JR_Data('',fullfile('Z:','QuailKit','data',d{i,1}),40,[0,10000,10],0.08);
+    k=0;
+    while true
+        k=k+1;
+        try
             [s,f, t] = obj.get((k-1)*10, k*10, 'spgram','1');
-            if isempty(s)
-                break
-            end
-            Template=importdata('Template_291.mat');
-            Calls = [Calls; SHdet(s,f,t,Template,0.296,0.525,false)];
+        catch
+            break
         end
-        l=size(Calls,1);
-        for i = 1:l
-            HT_DataAccess(handles,'query',...
-                ['insert into audio_detection([audio_id], [start], [end], [low], [high]) ',...
-                'values (''',char(string(Mics{k,4})),...
-                ''', ''',datestr(milliseconds(Calls(i,1)*1000)+handles.Data.Date,'yyyy-mm-ddTHH:MM:SS.FFF'),''', ''',datestr(milliseconds(Calls(i,2)*1000)+handles.Data.Date,'yyyy-mm-ddTHH:MM:SS.FFF'),...
-                ''', ',char(string(Calls(i,3))),', ',char(string(Calls(i,4))),')'],'cellarray');
+        calls = SHdet(s,f,t,291,0.296,0.525,false);
+        if ~isempty(calls)
+            calls.data_id=repmat(d{i,2},size(calls,1),1);
+            calls.channel=ones(size(calls,1),1);
+            sqlwrite(conn,'detection',calls);
         end
     end
 end
--- SHdet.m
+++ SHdet.m
@@ -1,97 +1,53 @@
-function varargout = SH_FindCalls(varargin)
-    % Input Parameters
-    % Spectrogram
-    % Template
-    % Thresh: Corresponding threshold for different templates
-    % Mode: Single (Single Envelope), Double (Double Envelope)
-    % h: Graphic handles (Line,Line,Scatter,Scatter)
-    
-    % Output:
-    % Contains number of calls (Number of rows)
-    % Each row contains start Time, End Time, Start freq. and End freq.
-    if nargin==0
-        varargout{1}={...
-            'Title','Quail Detector';...
-            'Author','Tsung-Sheng Huang';...
-            'Scale',0.525;...
-            'Threshold',0.296;...
-            'Double Pass',false};
-        varargout{2}=Visual();
-    else
-        S=varargin{1};
-        F=varargin{2};
-        t=varargin{3};
-        Template=varargin{4};
-        Thresh=varargin{5};
-        Distance=varargin{6};
-        DoublePass=varargin{7};
-        if nargin>7
-            h=varargin{8};
-        else
-            h=[];
-        end
-        [r,c]=size(Template);
-        % Template Matching
-        Upper = find(F==1000);
-        Downward = find(F==3000);
-        M = normxcorr2(Template,S(Upper:Downward,:));
-        M = M(r:size(M,1)-r+1,c:end);
-        Distance = Distance * 2;
-        Distance=round(Distance*numel(Template));
-        %     Calls = [];
-        %     CallsIDX = [];
-        if DoublePass
-            SecondDistance = round(Distance/3);
-            [Locs,Width] = TwoEnvelope(M,Distance,SecondDistance,Thresh,true,size(Template,2));
-            [StartF,StartT] = ind2sub(size(M),Locs);
-            StartF = StartF + 1000;
-            EndT = StartT + Width - 1;
-            EndF = StartF + size(Template,1)+1000;
-            EndT = round(EndT);
-            CallsIDX = [(StartT)',(EndT)',(StartF)',(EndF)'];
-            Calls = [t(StartT)',t(EndT)',F(StartF)',F(EndF)'];
-        else
-            [Locs,Width,up] = SingleEnvelope(M,Distance,Thresh,true);
-            Width = Width./(2*numel(Template));
-            Width = size(Template,2).*Width;
-            %         Width = size(Template,2);
-            [StartF,StartT] = ind2sub(size(M),Locs);
-            EndT = StartT + Width - 1;
-            EndF = StartF + size(Template,1)-1;
-            EndT = round(EndT);
-            EndT(EndT>size(M,2)) = size(M,2);
-            CallsIDX = [(StartT),(EndT),(StartF),(EndF)];
-            Calls = [t(StartT),t(EndT),F(StartF)'+1000,F(EndF)'+1000];
-        end
-        if ~isempty(h)
-            HT_BBPlot(t,F,S,Calls,h(2),[1,0,0],true);
-            Visual(h(3:end),M,up,Locs,Thresh);
-        end
-        if size(Calls,1)==0
-            Calls=[];
-        end
-        varargout{1}=Calls;
-        varargout{2}=CallsIDX;
-        varargout{3}=M;
-    end
-end
+function varargout = SHdet(S,F,t,tem,Thresh,Distance,DoublePass)
+% Input Parameters
+% Spectrogram
+% Template
+% Thresh: Corresponding threshold for different templates
+% Mode: Single (Single Envelope), Double (Double Envelope)
+% h: Graphic handles (Line,Line,Scatter,Scatter)
 
-function varargout=Visual(varargin)
-if nargin==0
-    varargout{1}={...
-        'Title','Cross-Correlation'};
+% Output:
+% Contains number of calls (Number of rows)
+% Each row contains start Time, End Time, Start freq. and End freq.
+Template=importdata(['template_',char(string(tem)),'.mat']);
+[r,c]=size(Template);
+% Template Matching
+Upper = find(F==1000);
+Downward = find(F==3000);
+M = normxcorr2(Template,S(Upper:Downward,:));
+M = M(r:size(M,1)-r+1,c:end);
+Distance = Distance * 2;
+Distance=round(Distance*numel(Template));
+%     Calls = [];
+%     CallsIDX = [];
+if DoublePass
+    SecondDistance = round(Distance/3);
+    [Locs,Width] = TwoEnvelope(M,Distance,SecondDistance,Thresh,true,size(Template,2));
+    [StartF,StartT] = ind2sub(size(M),Locs);
+    StartF = StartF + 1000;
+    EndT = StartT + Width - 1;
+    EndF = StartF + size(Template,1)+1000;
+    EndT = round(EndT);
+    CallsIDX = [(StartT)',(EndT)',(StartF)',(EndF)'];
+    Calls = [t(StartT)',t(EndT)',F(StartF)',F(EndF)'];
 else
-    h=varargin{1};
-    M=varargin{2};
-    up=varargin{3};
-    Locs=varargin{4};
-    Thresh=varargin{5};
-    set(h(1),'XData',1:numel(M),'YData',M(:));
-    set(h(2),'XData',1:numel(M),'YData',up);
-    set(h(3),'XData',Locs,'YData',M(Locs));
-    set(h(4),'XData',[1,numel(M)],'YData',Thresh*[1,1],'ZData',[0,0],...
-        'Visible','on');
+    [Locs,Width,up] = SingleEnvelope(M,Distance,Thresh,true);
+    Width = Width./(2*numel(Template));
+    Width = size(Template,2).*Width;
+    %         Width = size(Template,2);
+    [StartF,StartT] = ind2sub(size(M),Locs);
+    EndT = StartT + Width - 1;
+    EndF = StartF + size(Template,1)-1;
+    EndT = round(EndT);
+    EndT(EndT>size(M,2)) = size(M,2);
+    CallsIDX = [(StartT),(EndT),(StartF),(EndF)];
+    Calls = [t(StartT),t(EndT),F(StartF)'+1000,F(EndF)'+1000];
 end
+Calls=array2table(Calls,'VariableNames',{'start_t','end_t','low_f','high_f'});
+Calls.method=repmat({['name:SHTM2018','template:291','scale:',Distance,',threshold:',Thresh]},size(Calls,1),1);
+varargout{1}=Calls;
+varargout{2}=CallsIDX;
+varargout{3}=M;
 end
 
 function [locs,width,up] = SingleEnvelope(M,Distance,Thresh,UseDistance)
@@ -131,52 +87,52 @@ end
 
 function [locsE,WidthE] = TwoEnvelope(M,FirstDistance,SecondDistance,Thresh,UseDistance)
 
-    [up,~]=envelope(M(:),FirstDistance,'peak');  % First envelope
-    % Find the signal under the signal
-    [~,locs,Width,~] = findpeaks(up,1:numel(M));
+[up,~]=envelope(M(:),FirstDistance,'peak');  % First envelope
+% Find the signal under the signal
+[~,locs,Width,~] = findpeaks(up,1:numel(M));
 
-    % Avoid preallocation
-    Xidx = cell(size(locs));
-    MSub = cell(size(locs));
-    UpSub = cell(size(locs));
-    WidthE = cell(size(locs));
-    locsE = cell(size(locs));
+% Avoid preallocation
+Xidx = cell(size(locs));
+MSub = cell(size(locs));
+UpSub = cell(size(locs));
+WidthE = cell(size(locs));
+locsE = cell(size(locs));
 
-    for i=1:numel(locs)
-        idx = int64(locs(i)-Width(i)/2:locs(i)+Width(i)/2);
-        if numel(find(idx<=0))~=0
-            idx = idx(idx>0);
+for i=1:numel(locs)
+    idx = int64(locs(i)-Width(i)/2:locs(i)+Width(i)/2);
+    if numel(find(idx<=0))~=0
+        idx = idx(idx>0);
+    end
+    if numel(find(idx>numel(M)))~=0
+        idx = idx(idx<=numel(M));
+    end
+    if UseDistance==true
+        if numel(find(up(idx)>Thresh))<FirstDistance
+            Xidx{i} = int64([]);
+            locsE{i} = int64([]);
+            continue;
         end
-        if numel(find(idx>numel(M)))~=0
-            idx = idx(idx<=numel(M));
+        Xidx{i} = idx;
+        MSub{i} = M(idx);
+        [Temp,~] = envelope(M(idx),SecondDistance,'peak'); % Second Envelop
+        UpSub{i} = Temp;
+        locsTemp = int64(FindLocsUnderEnvelope(Temp,M(idx),Thresh,SecondDistance));
+        locsE{i} = locsTemp+idx(1)-1;
+        if numel(locsE{i}~=0)
+            WidthE{i} = repmat(Width(i),size(locsE{i}));
         end
-        if UseDistance==true
-            if numel(find(up(idx)>Thresh))<FirstDistance
-                Xidx{i} = int64([]);
-                locsE{i} = int64([]); 
-                continue;
-            end
-            Xidx{i} = idx;
-            MSub{i} = M(idx);
-            [Temp,~] = envelope(M(idx),SecondDistance,'peak'); % Second Envelop
-            UpSub{i} = Temp;
-            locsTemp = int64(FindLocsUnderEnvelope(Temp,M(idx),Thresh,SecondDistance)); 
-            locsE{i} = locsTemp+idx(1)-1;
-            if numel(locsE{i}~=0)
-                WidthE{i} = repmat(Width(i),size(locsE{i}));
-            end
-        else
-            Xidx{i} = idx;
-            MSub{i} = M(idx);
-            [Temp,~] = envelope(M(idx),SecondDistance,'peak'); % Second Envelop
-            UpSub{i} = Temp;
-            locsTemp = int64(FindLocsUnderEnvelope(Temp,M(idx),Thresh,0)); 
-            locsE{i} = locsTemp+idx(1)-1;
-            if numel(locsE{i}~=0)
-                WidthE{i} = repmat(Width(i),size(locsE{i}));
-            end
+    else
+        Xidx{i} = idx;
+        MSub{i} = M(idx);
+        [Temp,~] = envelope(M(idx),SecondDistance,'peak'); % Second Envelop
+        UpSub{i} = Temp;
+        locsTemp = int64(FindLocsUnderEnvelope(Temp,M(idx),Thresh,0));
+        locsE{i} = locsTemp+idx(1)-1;
+        if numel(locsE{i}~=0)
+            WidthE{i} = repmat(Width(i),size(locsE{i}));
         end
     end
-    locsE = cell2mat(locsE);
-    WidthE = cell2mat(WidthE);
+end
+locsE = cell2mat(locsE);
+WidthE = cell2mat(WidthE);
 end
